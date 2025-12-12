9baeec4b618176a5518b8ae46e62a6cca2187d35
Nikolaj Enggård Dahl <nda@vertica.dk>
2025-12-12 12:42:08 +0100

    Part 1 done

--- day-one/app.py
+++ day-one/app.py
@@ -1,16 +1,38 @@
-import filereader
 
 def get_number_from_line(line):
-    return int(line.split()[1])
+    if line.startswith('R'):
+        line = line.replace("R", "")
+        return int(line)
+    elif line.startswith('L'):
+        line = line.replace("L", "")
+        return int(line)
+    return 0
+
+def read_lines_from_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
 
 start_position = 50
 min_position = 0
 max_position = 99
+times_hit_min = 0
+lines = read_lines_from_file('puzzle_input.txt')
 
-lines = filereader.read_lines_from_file('input.txt')
-
+current_position = start_position
 for line in lines:
-    if line.startswith('L'):
-        start_position -= get_number_from_line(line)
-    elif line.startswith('R'):
-        start_position += get_number_from_line(line)
+    move_by = get_number_from_line(line)
+    print(f"Moving {line} by {move_by} from {current_position}")
+    for _ in range(move_by):
+        if line.startswith('R'):
+            current_position += 1
+            if current_position > max_position:
+                current_position = min_position
+        elif line.startswith('L'):
+            current_position -= 1
+            if current_position < min_position:
+                current_position = max_position
+    print(f"New current position: {current_position}")
+    if current_position == min_position:
+        times_hit_min += 1
+print(f"Times hit min position: {times_hit_min}")
